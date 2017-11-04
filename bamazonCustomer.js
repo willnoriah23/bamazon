b45e4e522387eb3c522ae061e63b44b1d2c35830
Noriah Williams <willnoriah23@gmail.com>
2017-11-04 02:13:55 -0400

    Completed homework

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -42,8 +42,8 @@ function selectTable() {
       type: "input",
       message: "How many would you like to purchase?"
     }]).then(function(answer) {
-      var productId = answer.id 
-      var quantityAmt = answer.quantity
+      var productId = answer.id; 
+      var quantityAmt = answer.quantity;
       
       connection.query("SELECT * FROM products WHERE ?",
        [
@@ -52,34 +52,26 @@ function selectTable() {
           // Why aren't we including --> stock.quantity: quantityAmt
         }
        ],function(err, res) {
-          console.log(res); 
+          //console.log(res); 
           console.log(parseInt(res[0].stock_quantity));
           // var test = parseInt(res[0].stock_quantity) - quantityAmt;
           // console.log(test);
           // console.log(quantityAmt);
           if ((parseInt(res[0].stock_quantity) - parseInt(quantityAmt)) > 0) {
+            console.log("We have your item in stock, you may proceed with your purchase."); 
             connection.query("UPDATE products SET ? WHERE ?", [{
               stock_quantity: res[0].stock_quantity - quantityAmt
             }, {
               item_id: productId
 
             }, function(err, res) {
-              if (res < 
-
+              if (err) throw err;
+              //checkAvail(parseInt(quantityAmt), parseInt(res[0].stock_quantity));
             }]) 
       connection.end();
-          }
-       })
-  })     
+        } else {
+              console.log("Insufficient quantity in stock.");
+        }
+     })
+  })    
 };
-
-// 7. Once the customer has placed the order, your application should check 
-// if your store has enough of the product to meet the customer's request.
-
-// * If not, the app should log a phrase like `Insufficient quantity!`, 
-// and then prevent the order from going through.
-
-// 8. However, if your store _does_ have enough of the product, 
-//you should fulfill the customer's order.
-// * This means updating the SQL database to reflect the remaining quantity.
-// * Once the update goes through, show the customer the total cost of their purchase.
